d36be1cba058a1610cff2557b299d73192e26835
Teddy Foley <tef77777@gmail.com>
2016-06-20 18:50:09 +0000

    Done.

--- ruby_phone_format.rb
+++ ruby_phone_format.rb
@@ -1,1 +1,17 @@
-# Write your code here
+# make method that validates integers
+def int_validation(value)
+  if (value == value.to_i.to_s)
+    true
+  else
+    false
+  end
+end
+
+puts "Please input your phone number, starting with the first number now."
+user_value = gets.chomp()
+
+if int_validation(user_value)
+  puts "Your first number is #{user_value}"
+else
+  puts "Your input was not a valid integer."
+end
